6f7bd05d2c109f8c5ad3dc01e1d0bce32a91e8f8
kderrylo <derrylzodxyt@gmail.com>
2024-11-13 18:05:21 +0700

    first commit

---
+++ chatLlama3.2-1b.py
@@ -0,0 +1,59 @@
+import streamlit as st
+from langchain_ollama import ChatOllama
+from langchain_core.output_parsers import StrOutputParser
+from langchain.prompts import SystemMessagePromptTemplate, HumanMessagePromptTemplate, AIMessagePromptTemplate, ChatPromptTemplate
+
+st.title("Chatbot using Llama 3.2:1b 💬")
+st.write("Github Repository: https://github.com/kderrylo?tab=repositories")
+
+model = ChatOllama(model="llama2", base_url="http://localhost:11434") # load model dari ollama
+
+# spesifikasi interaksi dengan AI assistant
+system_message = SystemMessagePromptTemplate.from_template("You are AI assistant, explain things in 1 sentence")
+
+# atur state untuk history chat
+if "chat_history" not in st.session_state:
+    st.session_state.chat_history = []
+def history():
+    chat_history = [system_message]
+    for chat in st.session_state.chat_history:
+        chat_history.append(HumanMessagePromptTemplate.from_template(chat['user']))
+        chat_history.append(AIMessagePromptTemplate.from_template(chat["assistant"]))
+    return chat_history
+
+# buat form
+with st.form("form"):
+    input = st.text_area("Input your text/prompt here 👇👇 !", key="user_input")
+    button = st.form_submit_button("Enter")
+
+# AI response generation
+def response_generate(chat_history):
+    chat_template = ChatPromptTemplate.from_messages(chat_history)
+    # model_response = model.invoke(chat_template)
+    model_response = chat_template|model|StrOutputParser()
+    return model_response.invoke({})
+    # chain = chat_template | model | StrOutputParser()
+    # return chain.invoke({})
+
+if input and button:
+    with st.spinner("Thinking . . . 🤔💭"):
+        chat_history = history()
+        
+        chat_history.append(HumanMessagePromptTemplate.from_template(input))
+
+        response = response_generate(chat_history)
+        if response:
+            st.session_state.chat_history.append({
+                "user":input,
+                "assistant":response
+            })
+
+st.write("---")
+st.write("### Chat History ⏳")
+for chat in reversed(st.session_state["chat_history"]):
+    st.write(f"😃 You : {chat['user']}")
+    st.write(f"🤖  AI : {chat['assistant']}")
+    st.write("---")
+
+
+
